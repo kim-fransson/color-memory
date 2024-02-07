9029315c23aed02c4c1e278989947a5251c45360
Kim Fransson <kim.fransson@sigmatechnology.se>
2024-02-07 16:58:54 +0100

    fix(mobile): added active colors when pressing buttons

--- src/components/buttons/QuadrantButton/QuadrantButton.tsx
+++ src/components/buttons/QuadrantButton/QuadrantButton.tsx
@@ -1,77 +1,94 @@
-import { cva, VariantProps } from 'class-variance-authority';
-import { Button as AriaButton, ButtonProps as AriaButtonProps } from 'react-aria-components';
-import { twJoin } from 'tailwind-merge';
+import { cva, VariantProps } from "class-variance-authority";
+import {
+  Button as AriaButton,
+  ButtonProps as AriaButtonProps,
+} from "react-aria-components";
+import { twJoin } from "tailwind-merge";
 
-const button = cva([
-  "outline-none pressed:scale-95 transition-all rounded-tr-full min-w-8 min-h-8",
-], {
-  variants: {
-    state: {
-      active: [],
-      inactive: []
-    },
-    color: {
-      green: ["-rotate-90 hover:bg-green focus-visible:bg-green"],
-      red: ["hover:bg-red focus-visible:bg-red"],
-      blue: ["hover:bg-blue focus-visible:bg-blue rotate-90"],
-      yellow: ["hover:bg-light-orange focus-visible:bg-light-orange  rotate-180"]
-    }
-  },
-  compoundVariants: [
-    {
-      state: "active",
-      color: "green",
-      className: "bg-green",
-    },
-    {
-      state: "inactive",
-      color: "green",
-      className: "bg-[#5d9c6e]",
-    },
-    {
-      state: "active",
-      color: "red",
-      className: "bg-red"
-    },
-    {
-      state: 'inactive',
-      color: 'red',
-      className: "bg-[#935457]"
-    },
-    {
-      state: "active",
-      color: "blue",
-      className: "bg-blue"
-    },
-    {
-      state: "inactive",
-      color: "blue",
-      className: "bg-[#487499]"
-    },
-    {
-      state: "active",
-      color: "yellow",
-      className: "bg-light-orange"
-    },
-    {
-      state: 'inactive',
-      color: "yellow",
-      className: "bg-[#8d7345]"
-    }
+const button = cva(
+  [
+    "outline-none pressed:scale-95 transition-all rounded-tr-full min-w-8 min-h-8",
   ],
-  defaultVariants: {
-    state: 'inactive',
+  {
+    variants: {
+      state: {
+        active: [],
+        inactive: [],
+      },
+      color: {
+        green: [
+          "-rotate-90 hover:bg-green focus-visible:bg-green pressed:bg-green",
+        ],
+        red: ["hover:bg-red focus-visible:bg-red pressed:bg-red"],
+        blue: ["hover:bg-blue focus-visible:bg-blue rotate-90 pressed:blue"],
+        yellow: [
+          "hover:bg-light-orange focus-visible:bg-light-orange pressed:bg-light-orange rotate-180",
+        ],
+      },
+    },
+    compoundVariants: [
+      {
+        state: "active",
+        color: "green",
+        className: "bg-green",
+      },
+      {
+        state: "inactive",
+        color: "green",
+        className: "bg-[#5d9c6e]",
+      },
+      {
+        state: "active",
+        color: "red",
+        className: "bg-red",
+      },
+      {
+        state: "inactive",
+        color: "red",
+        className: "bg-[#935457]",
+      },
+      {
+        state: "active",
+        color: "blue",
+        className: "bg-blue",
+      },
+      {
+        state: "inactive",
+        color: "blue",
+        className: "bg-[#487499]",
+      },
+      {
+        state: "active",
+        color: "yellow",
+        className: "bg-light-orange",
+      },
+      {
+        state: "inactive",
+        color: "yellow",
+        className: "bg-[#8d7345]",
+      },
+    ],
+    defaultVariants: {
+      state: "inactive",
+    },
   },
-})
+);
 
 type QuadrantButtonProps = {
   className?: string;
-} & Omit<AriaButtonProps, 'children'> & VariantProps<typeof button>
+} & Omit<AriaButtonProps, "children"> &
+  VariantProps<typeof button>;
 
-export const QuadrantButton = ({ color, state, className, ...reactAriaProps }: QuadrantButtonProps) => {
+export const QuadrantButton = ({
+  color,
+  state,
+  className,
+  ...reactAriaProps
+}: QuadrantButtonProps) => {
   return (
     <AriaButton
-      {...reactAriaProps} className={twJoin(button({ color, state }), className)}
+      {...reactAriaProps}
+      className={twJoin(button({ color, state }), className)}
     />
-  )
-}
+  );
+};
